c91af7381fd46f2bda09ade7f279f4dcc690c054
ngocthangictu <ngocthang.ictu@gmail.com>
2014-12-03 14:55:51 +0700

    dsad

--- src/controllers/CpanelController.php
+++ src/controllers/CpanelController.php
@@ -2,10 +2,6 @@
 class CpanelController extends Controller{
     
     function get_index(){
-<<<<<<< HEAD
         return View::make('administrator::dashboard');
-=======
-        return View::make('administrator::home');
->>>>>>> 1e3005f3b2d7ff6fd34ec9e837552bf9feaf4051
     }
 }
